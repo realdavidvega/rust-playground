b7662b9e2535aa40d16634e92e862bb0e61b9d50
David Vega Lichacz <davidvegalichacz@gmail.com>
2022-07-23 21:04:59 +0200

    fix(tcp_chat_server): clean the buffer

--- 03_tcp_chat_server/src/main.rs
+++ 03_tcp_chat_server/src/main.rs
@@ -26,13 +26,16 @@ async fn main() {
     // }
 
     let (reader, mut writer) = socket.split();
+
     let mut reader = BufReader::new(reader);
     let mut line = String::new();
+
     loop {
         let bytes_read = reader.read_line(&mut line).await.unwrap();
         if bytes_read == 0 {
             break;
         }
         writer.write_all(line.as_bytes()).await.unwrap();
+        line.clear();
     }
 }
