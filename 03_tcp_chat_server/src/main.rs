92e8f90a7464d4382eac959215ab64bcad1d1437
David Vega Lichacz <davidvegalichacz@gmail.com>
2022-07-23 20:47:49 +0200

    feat(tcp_chat_server): migrate to BufReader

--- 03_tcp_chat_server/src/main.rs
+++ 03_tcp_chat_server/src/main.rs
@@ -1,4 +1,4 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
 
 // we tell rust to use the macro for tokyo async main, which uses futures (non-known values yet)
@@ -13,12 +13,26 @@ async fn main() {
     // accept and address new connection
     let (mut socket, _addr) = listener.accept().await.unwrap();
 
-    // data buffer
-    let mut buffer = [0u8; 1024];
+    // basic data buffer read and write
+    // loop {
+    //     // data buffer
+    //     let mut buffer = [0u8; 1024];
+    //
+    //     // read into the buffer
+    //     let bytes_read = socket.read(&mut buffer).await.unwrap();
+    //
+    //     // writes all bytes from the input buffer to the output buffer
+    //     socket.write_all(&buffer[..bytes_read]).await.unwrap();
+    // }
 
-    // read into the buffer
-    let bytes_read = socket.read(&mut buffer).await.unwrap();
-
-    // writes all bytes from the input buffer to the output buffer
-    socket.write_all(&buffer[..bytes_read]).await.unwrap();
+    let (reader, mut writer) = socket.split();
+    let mut reader = BufReader::new(reader);
+    let mut line = String::new();
+    loop {
+        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        if bytes_read == 0 {
+            break;
+        }
+        writer.write_all(line.as_bytes()).await.unwrap();
+    }
 }
