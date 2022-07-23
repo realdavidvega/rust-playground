239fa0af0907ed38c5f86747e198854ad90fb286
David Vega Lichacz <davidvegalichacz@gmail.com>
2022-07-23 20:23:28 +0200

    feat(tcp_chat_server): reading and writing messages from the buffer at the tcp listener

--- 03_tcp_chat_server/src/main.rs
+++ 03_tcp_chat_server/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+// we tell rust to use the macro for tokyo async main, which uses futures (non-known values yet)
+#[tokio::main]
+async fn main() {
+    // tcp echo server
+    let addr = "[::1]:8080";
+
+    // returns a future which we wait and unwrap the result
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    // accept and address new connection
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+
+    // data buffer
+    let mut buffer = [0u8; 1024];
+
+    // read into the buffer
+    let bytes_read = socket.read(&mut buffer).await.unwrap();
+
+    // writes all bytes from the input buffer to the output buffer
+    socket.write_all(&buffer[..bytes_read]).await.unwrap();
 }
