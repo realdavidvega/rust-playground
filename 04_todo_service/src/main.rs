fd1cafef81edf22fa9c627a4caec046a6c20d091
David Vega Lichacz <davidvegalichacz@gmail.com>
2022-07-24 20:49:30 +0200

    feat(todo_service): add first endpoint

--- 04_todo_service/src/main.rs
+++ 04_todo_service/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    rocket::ignite()
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
